198c265afbdc1286835e1f37b8dcdc054d176c72
Veretion <48298825+Veretion@users.noreply.github.com>
2021-02-26 06:59:14 +0300

    Add files via upload

---
+++ settings_and_variables.py
@@ -0,0 +1,32 @@
+username = ""
+password = ""
+
+webpage = ""
+web_msg = ""
+
+# закрываем всплывающее окно (данные для входа)
+window_check1 = "/html/body/div[3]/div/div"
+window_close1 = "/html/body/div[3]/div/div/div/div[3]/button[2]"
+# находим кнопку "отправить сообщение"
+send_message1 = "/html/body/div[1]/section/div/div[2]/div/div/div[1]/div[1]/div/div[3]/button"
+send_message2 = "/html/body/div[1]/section/div/div[2]/div/div/div[1]/div[1]/div/div[3]/button"
+
+# кликаем по первому пользователю в списке
+user_1 = "/html/body/div[5]/div/div/div[2]/div[2]/div[1]"
+user_2 = "/html/body/div[5]/div/div/div[2]/div[2]/div[2]"
+user_x = "/html/body/div[2]/div/div/div[2]/div[2]/div[2]"
+user_d = "/html/body/div[4]/div/div/div[2]/div[2]"
+
+ochnepeb_1 = "/html/body/div[5]/div/div/div[2]/div[2]/div[1]"  # Ochen_neponyatnaya_poeben_1
+ochnepeb_2 = "/html/body/div[5]/div/div/div[2]/div[2]/div[2]"  # Ochen_neponyatnaya_poeben_2
+
+
+# находим и нажимаем кнопку "далее"
+BFS = "/html/body/div[4]"  # button_further_search
+BFP = "/html/body/div[5]/div/div/div[1]/div/div[2]/div/button"  # button_further_press
+
+send_message_area = "/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea"
+message = 'hi'
+
+
+
